6b98c4287c5373d123d1473cbfe701016c06ac23
arcvin <arcvin.crop@gmail.com>
2023-07-10 19:27:22 +0530

    Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fa, faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'typing-game';
+export class AppComponent implements OnInit {
+  fakeSentence = [''];
+  userInput = [''];
+
+  ngOnInit(): void {
+    this.generateFakeSentence();
+  }
+
+  generateFakeSentence() {
+    this.fakeSentence = faker.lorem.sentence().split('');
+  }
+
+  compare(event: Event) {
+    this.userInput = (event.target as HTMLInputElement).value.split('');
+  }
+
+  getColorClass(index: number) {
+    if (this.userInput[index] === undefined || this.userInput[index] === '')
+      return 'grey';
+
+    return this.fakeSentence[index] === this.userInput[index]
+      ? 'correct'
+      : 'incorrect';
+  }
+
+  compareSentences() {
+    return this.userInput.join('') === this.fakeSentence.join('')
+      ? true
+      : false;
+  }
 }
